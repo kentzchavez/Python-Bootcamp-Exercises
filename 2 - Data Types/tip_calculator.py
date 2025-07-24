142f0ad5ec2cdb168eeada1754f5339f94261621
KC Chavez <kcchavez@KCs-MacBook-Air.local>
2025-07-24 14:25:27 +0800

    Completed Day 2 - Data Types

---
+++ 2 - Data Types/tip_calculator.py
@@ -0,0 +1,9 @@
+# Variables provided by the course
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+
+# Calculation
+amount = round(((bill/people) * ((tip/100)+1)),2)
+print(f"\n[] Each person should pay: ${amount}")
