6f84148baf4c0ad2d211405b63fdea15138ce913
KC Chavez <kcchavez@KCs-MacBook-Air.local>
2025-07-24 13:49:04 +0800

    Completed Day 1 - Var

---
+++ 1 - Variables/band_name_gen.py
@@ -0,0 +1,8 @@
+print("Let's generate your band name!")
+
+# Get inputs
+city = input("[] What is the name of the city that you grew up in?\n>> ").capitalize()
+pet = input("[] Give me a name of a pet.\n>> ").capitalize()
+
+# Print band name
+print(f"\n[] Your band name is: {city} {pet}!")
